930ab98a89d6f67ad3f5d2663a25c8723f9af8cf
Ramez Hasnaoui <ramez.hassnaoui@iteam-univ.tn>
2025-04-15 17:54:45 +0100

    Merge remote-tracking branch 'origin/master'

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask,request,jsonify
 from blockchain import Blockchain
+import hashlib
 app = Flask(__name__)
 blockchain=Blockchain()
-@app.route('/')
-def index():
-    return 'Blockchain voting API is working!'
+voted_ids = set()
 
 @app.route('/vote', methods=['POST'])
 def vote():
     data = request.get_json()
     voterId = data.get('voterId')
-    
+    vote = data.get('vote')
+    voter_hash = hashlib.sha256(voterId.encode()).hexdigest()
+
+    if voter_hash in voted_ids:
+        return jsonify({'message': 'You have already voted!'}), 400
+
+    blockchain.current_transactions.append({'voter': voter_hash, 'vote': vote})
+    voted_ids.add(voter_hash)
+
+    return jsonify({'message': 'Vote successfully recorded.'}), 200
