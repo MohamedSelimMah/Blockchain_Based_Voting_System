16005a9d898828a646f1177caa26b06f83b81874
MohamedSelimMah <mohamedselim.mahjoub@iteam-univ.tn>
2025-04-15 21:07:02 +0100

    commited

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -2,28 +2,31 @@ import hashlib
 from time import time
 import json
 
+import requests
+
+
 class Blockchain:
     def __init__(self):
-        self.current_transactions =[]
+        self.new_transactions =[]
         self.chain = []
         self.nodes = set()
 
-        self.new_block(proof='100',previous_hash='1')
+        self.new_block(proof=100,previous_hash='1')
 
     def new_block(self,proof,previous_hash= None):
         block={
             'index': len(self.chain) + 1,
             'timestamp': time(),
-            'transactions':self.current_transactions,
+            'transactions':self.new_transactions,
             'proof':proof,
             'previous_hash':previous_hash or self.hash(self.chain[-1]),
         }
-        self.current_transactions = []
+        self.new_transactions = []
         self.chain.append(block)
         return block
 
-    def current_transactions(self,sender,amount,recipient):
-        self.current_transactions.append({
+    def new_transactions(self,sender,amount,recipient):
+        self.new_transactions.append({
             'sender': sender,
             'recipient': recipient,
             'amount': amount,
@@ -52,4 +55,43 @@ class Blockchain:
         g_hash=hashlib.sha256(g).hexdigest()
         return g_hash[:4] == "0000"
 
+    def valid_chain(self,chain):
+        last_block = chain[0]
+        current_index = 1
 
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            if not self.valid_proof(last_block['proof'],block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+        return True
+
+    def resolve_conflicts(self):
+
+        neighbours = self.nodes
+        new_chain =None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
