03f7a0c48e54d165fa829f52158df6e5c5143b33
MohamedSelimMah <mohamedselim.mahjoub@iteam-univ.tn>
2025-04-23 23:02:03 +0100

    commited

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -73,22 +73,22 @@ class Blockchain:
         return True
 
     def resolve_conflicts(self):
-
         neighbours = self.nodes
-        new_chain =None
-
+        new_chain = None
         max_length = len(self.chain)
 
         for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+            try:
+                response = requests.get(f'http://{node}/chain')
+                if response.status_code == 200:
+                    length = response.json()['length']
+                    chain = response.json()['chain']
 
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
-
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+                    if length > max_length and self.valid_chain(chain):
+                        max_length = length
+                        new_chain = chain
+            except Exception as e:
+                print(f"[ERROR] Failed to connect to node {node}: {e}")
 
         if new_chain:
             self.chain = new_chain
